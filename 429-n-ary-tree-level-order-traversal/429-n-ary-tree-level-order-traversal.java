b44c82bd3d1e8c69be3dc4ee26fe88a6f56e8651
Anand Raj <77964578+raj00anand@users.noreply.github.com>
2022-09-05 16:14:07 +0530

    Time: 2 ms (95.52%), Space: 46.8 MB (77.77%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -21,22 +21,20 @@ class Solution {
     public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> ans=new ArrayList<>();
         if(root==null) return ans;
-        Queue<Node> que=new ArrayDeque<>();
-        que.add(root);
-        while(que.size()>0){
-            List<Integer> level=new ArrayList<>();
-            int size=que.size();
-            for(int i=0;i<size;i++){
-                Node rn=que.remove();
-                level.add(rn.val);
-                for(int j=0;j<rn.children.size();j++){
-                    que.add(rn.children.get(j));
-                }
-                
-            }
-            ans.add(level);
-        }
+        solve(root, 0, ans);
         return ans;
+    }
+    public void solve(Node node,int level, List<List<Integer>> ans){
+        if(node==null) return;
+        
+        if(level>=ans.size()){
+            ans.add(new ArrayList<>());
+        }
+        ans.get(level).add(node.val);
+        
+        for(Node n: node.children){
+            solve(n, level+1, ans);
+        }
     }
     
 }
