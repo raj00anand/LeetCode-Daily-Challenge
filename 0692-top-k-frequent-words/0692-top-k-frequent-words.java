5ed32cfdeab8ec09098d40dfc79e919d900b4a08
Anand Raj <77964578+raj00anand@users.noreply.github.com>
2022-10-19 11:55:22 +0530

    Time: 19 ms (8.99%), Space: 45.5 MB (24.62%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,25 +1,11 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        TreeMap<String, Integer> mp = new TreeMap();
+        TreeMap<String, Integer> map=new TreeMap<>();
         for(String str: words){
-            mp.put(str, mp.getOrDefault(str, 0)+1);
+            map.put(str, map.getOrDefault(str, 0)+1);
         }
-        PriorityQueue<Map.Entry<String,Integer>> pq = new PriorityQueue( new Comparator<Map.Entry<String,Integer>>(){
-            @Override
-            public int compare(Map.Entry<String,Integer> e1, Map.Entry<String,Integer> e2){
-                return e1.getValue() != e2.getValue()? e1.getValue() - e2.getValue():e2.getKey().compareTo(e1.getKey());
-            }
-        });
-        for(Map.Entry<String,Integer> et:mp.entrySet()){
-            pq.offer(et);
-            if(pq.size()>k){
-                pq.poll();
-            }
-        }
-        List<String> res = new LinkedList();
-        while(!pq.isEmpty()){
-           res.add(0,pq.poll().getKey());
-       }
-        return res;
+        List<String> ans=new ArrayList<>(map.keySet());
+        Collections.sort(ans, (a,b)->map.get(b)-map.get(a));
+        return ans.subList(0, k);
     }
 }
