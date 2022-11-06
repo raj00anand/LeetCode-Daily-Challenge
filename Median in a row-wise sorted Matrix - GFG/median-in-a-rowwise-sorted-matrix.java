ba6942f8dc38248d10178bc8fc0e36f72fc5e1d4
Anand Raj <77964578+raj00anand@users.noreply.github.com>
2022-11-06 17:04:44 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -38,6 +38,50 @@ class GFG
 
 class Solution {
     int median(int matrix[][], int R, int C) {
+        // code here
+        int max=Integer.MIN_VALUE, min=Integer.MAX_VALUE;
+        for(int i=0, j=C-1;i<R;i++){
+            if(max<matrix[i][j]){
+                max=matrix[i][j];
+            }
+        }
+        for(int i=0, j=0;i<R;i++){
+            if(min>matrix[i][j]){
+                min=matrix[i][j];
+            }
+        }
+        int desired=(R*C+1)/2;
+        while(min<max){
+            int mid=min+(max-min)/2;
+            
+            int place=0;
+            int get=0;
+            
+            for(int i=0;i<R;i++){
+                get=Arrays.binarySearch(matrix[i], mid);
+                if(get<0){
+                    get=Math.abs(get)-1;
+                }else{
+                    while(get<matrix[i].length && matrix[i][get]==mid){
+                        get=get+1;
+                    }
+                }
+                place=place+get;
+            }
+            if(place<desired){
+                min=mid+1;
+            }else{
+                max=mid;
+            }
+        }
+        return min;
+        
+    }
+}
+
+
+/*class Solution {
+    int median(int matrix[][], int R, int C) {
         // code here 
         int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
         for(int i=0, j=0;i<R;i++){
@@ -79,4 +123,4 @@ class Solution {
         return min;
         
     }
-}
+}*/
