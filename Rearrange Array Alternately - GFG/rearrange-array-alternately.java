fca8603383ed2982c9e694d59065410a24d2056c
Anand Raj <77964578+raj00anand@users.noreply.github.com>
2022-12-01 20:23:45 +0530

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        // Your code here
+        
+        PriorityQueue<Long> pq1=new PriorityQueue<>();
+        PriorityQueue<Long> pq2=new PriorityQueue<>(Collections.reverseOrder());
+        
+        long ans[]=new long[n];
+        int i=0;
+        int l=0;
+        int m=arr.length-1;
+        while(i<n){
+            ans[i++]=arr[m--];
+            if(i<n){
+                ans[i++]=arr[l++];
+            }
+        }
+        for(int j=0;j<n;j++){
+            arr[j]=ans[j];
+        }
+       
+        
+    }
+    
+}
+
+
