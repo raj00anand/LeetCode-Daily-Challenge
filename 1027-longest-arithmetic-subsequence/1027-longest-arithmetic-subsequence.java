8d2e99eaca75069ee72ca44d48054c9a22754114
Anand Raj <ar.anandraj02@gmail.com>
2023-06-23 22:44:56 +0530

    Time: 660 ms (41.64%), Space: 69.2 MB (40.88%) - LeetHub

---
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestArithSeqLength(int[] nums) {
+        int maxLen = 0;
+        Map<Integer, Integer> dp[]=new HashMap[nums.length];
+        for(int i=0;i<nums.length;i++){
+            dp[i]=new HashMap<>();
+            for(int j=0;j<i;j++){
+                int diff = nums[j]-nums[i];
+                dp[i].put(diff, dp[j].getOrDefault(diff, 1)+1);
+                maxLen = Math.max(maxLen, dp[i].get(diff));
+                
+            }
+        }
+        return maxLen;
+    }
+}
