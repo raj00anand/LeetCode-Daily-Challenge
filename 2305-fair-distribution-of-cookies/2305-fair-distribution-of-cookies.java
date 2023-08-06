332329a3a1fdf27117913644a726f49c85e9eac4
Anand Raj <ar.anandraj02@gmail.com>
2023-08-07 02:06:31 +0530

    Time: 15 ms (79.46%), Space: 40.1 MB (41.13%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,41 +1,29 @@
 class Solution {
-    private int dfs(int i, int[] distribute, int[] cookies, int k, int zeroCount) {
-        // If there are not enough cookies remaining, return Integer.MAX_VALUE
-        // as it leads to an invalid distribution.
-        if (cookies.length - i < zeroCount) {
-            return Integer.MAX_VALUE;   
+    private int solve(int i, int distribute[], int cookies[], int k, int zeroCount){
+        if(cookies.length - i < zeroCount){
+            return Integer.MAX_VALUE;
         }
-
-        // After distributing all cookies, return the unfairness of this
-        // distribution.
-        if (i == cookies.length) {
-            int unfairness = Integer.MIN_VALUE;
-            for (int value : distribute) {
-                unfairness = Math.max(unfairness, value);
+        
+        if(i==cookies.length){
+            int fairness = Integer.MIN_VALUE;
+            for(int val: distribute){
+                fairness = Math.max(val, fairness);
             }
-            return unfairness;
+            return fairness;
         }
         
-        // Try to distribute the i-th cookie to each child, and update answer
-        // as the minimum unfairness in these distributions.
-        int answer = Integer.MAX_VALUE;
-        for (int j = 0; j < k; ++j) {
-            zeroCount -= distribute[j] == 0 ? 1 : 0;
+        int ans = Integer.MAX_VALUE;
+        for(int j=0;j<k;j++){
+            zeroCount -= distribute[j]==0 ? 1: 0;
             distribute[j] += cookies[i];
-            
-            // Recursively distribute the next cookie.
-            answer = Math.min(answer, dfs(i + 1, distribute, cookies, k, zeroCount));
-            
+            ans = Math.min(ans, solve(i+1, distribute, cookies, k, zeroCount));
             distribute[j] -= cookies[i];
-            zeroCount += distribute[j] == 0 ? 1 : 0;
+            zeroCount += distribute[j]==0 ? 1: 0;
         }
-        
-        return answer;
+        return ans;
     }
-    
     public int distributeCookies(int[] cookies, int k) {
-        int[] distribute = new int[k];
-        
-        return dfs(0, distribute, cookies, k, k);
+        int distribute[]=new int[k];
+        return solve(0, distribute, cookies, k, k);
     }
 }
