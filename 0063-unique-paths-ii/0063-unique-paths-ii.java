14a87005bb9d5f3ccaca8a2dac880bc5c2809378
Anand Raj <ar.anandraj02@gmail.com>
2023-08-13 02:46:32 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (89.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,27 +1,28 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n= obstacleGrid.length;
-        int m= obstacleGrid[0].length;
-        int dp[][]=new int[n+1][m+1];
-        return solve(0, 0, obstacleGrid, n, m, dp);
-    }
-    public int solve(int i, int j, int[][] obstacleGrid, int n,int m, int dp[][]){
-        if(i<0 || i>=n || j<0 || j>=m || obstacleGrid[i][j]==1){
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        
+        if(obstacleGrid[m-1][n-1] == 1){
             return 0;
         }
-        if(i==n-1 && j==m-1){
-            return 1;
+        int dp[][]=new int[m][n];
+        // dp[]
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(obstacleGrid[i][j]==0){
+                    if(i==m-1 && j==n-1){
+                        dp[i][j]=1;
+                    }else if(i==m-1){
+                        dp[i][j]=dp[i][j+1];
+                    }else if(j==n-1){
+                        dp[i][j]=dp[i+1][j];
+                    }else{
+                        dp[i][j]=dp[i+1][j]+dp[i][j+1];
+                    }
+                }
+            }
         }
-        if(dp[i][j]!=0){
-            return dp[i][j];
-        }
-        int count=0;
-        
-        count += solve(i+1, j, obstacleGrid, n, m, dp);
-        count += solve(i, j+1, obstacleGrid, n, m, dp);
-        
-        dp[i][j]=count;
-        
-        return count;
+        return dp[0][0];
     }
 }
