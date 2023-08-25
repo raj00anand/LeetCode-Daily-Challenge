d04f93e124311aa525355ebb4ddd7282f409162b
Anand Raj <ar.anandraj02@gmail.com>
2023-08-25 12:03:52 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (73.29%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        
+        int inc = nums[0];
+        int exc = 0;
+        
+        for(int i=1;i<n;i++){
+            int ninc = exc+nums[i];
+            int nexc = Math.max(inc, exc);
+            
+            inc = ninc;
+            exc = nexc;
+        }
+        return Math.max(inc, exc);
+    }
+}
